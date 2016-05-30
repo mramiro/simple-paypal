85ed0eaf7578e98920c405a0d1917ab6237e4b31
Miguel Ramiro <miguel@mramiro.com>
2016-05-30 18:10:38 -0500

    Moved the template rendering engine to Twig

--- src/Buttons/Button.php
+++ src/Buttons/Button.php
@@ -1,17 +1,24 @@
 <?php namespace SimplePaypal\Buttons;
 
+use Twig_Environment;
+use Twig_Loader_Filesystem;
+
 abstract class Button extends VarCollection
 {
+  const TEMPLATE_DIR = __DIR__.'/theme';
+
   protected static $allowed = array(
     'cmd',
     'notify_url',
     'bn'
   );
   protected $renderer;
-  protected $label = 'Pay with Paypal';
   protected $formAction;
   protected $formTarget = '_blank';
   protected $buttonType = '';
+  protected $buttonLabel = 'Pay with Paypal';
+  protected $buttonSize = 'large';
+  protected $buttonStyle = null;
 
   protected function getAllowedVars()
   {
@@ -47,11 +54,18 @@ abstract class Button extends VarCollection
 
   public function toHtmlForm()
   {
-    ob_start();
-    include __DIR__.'/theme/template.php';
-    $html = ob_get_contents();
-    ob_end_clean();
-    return $html;
+    if (!isset($this->renderer)) {
+      $this->renderer = new Twig_Environment(new Twig_Loader_Filesystem(static::TEMPLATE_DIR));
+    }
+    $params = array(
+      'action' => $this->formAction,
+      'target' => $this->formTarget,
+      'vars' => $this->getVars(),
+      'size' => $this->buttonSize,
+      'style' => $this->buttonStyle,
+      'label' => $this->buttonLabel,
+    );
+    return $this->renderer->render('template.twig', $params);
   }
 
   public function __toString()
--- src/Buttons/theme/template.php
+++
@@ -1,6 +0,0 @@
-<form class="paypal-button" method="POST" action="<?= $this->formAction ?>" target="<?= $this->formTarget ?>">
-<?php foreach ($this->getVars() as $key => $value) { ?>
-<input type="hidden" name="<?= $key ?>" value="<?= $value ?>">
-<?php } ?>
-<button class="paypal-button large" type="submit"><?= $this->label ?></button>
-</form>
