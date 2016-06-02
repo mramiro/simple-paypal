69f5efb8b83a35862cff74d0663b742d28c07588
Miguel Ramiro <miguel@mramiro.com>
2016-06-02 12:36:15 -0500

    Added gulp. Added gulp task for compiling less to css. Created stylesheet for buttons.

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var rename = require('gulp-rename');
+var cleancss = require('gulp-clean-css');
+var autoprefixer = require('gulp-autoprefixer');
+
+var lessPath = 'src/Buttons/template/less';
+var lessEntry = lessPath + '/*.less';
+var lessAll = lessPath + '/**/*.less';
+var buildPath = 'dist';
+
+gulp.task('css', function(){
+  return gulp.src(lessEntry)
+    .pipe(less()).on('error', function(error) {
+      console.log(error.toString());
+      this.emit('end');
+    })
+    .pipe(autoprefixer({ browsers: ['last 2 versions'] }))
+    .pipe(cleancss())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest(buildPath));
+});
+
+gulp.task('watch', function() {
+  return gulp.watch(lessAll, ['css']);
+});
